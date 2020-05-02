b3e13403d2e1ec0b402800a1ba2da394cbcc1534
Nicolai Finstad Larsen <59092220+NicolaiFinstadLarsen@users.noreply.github.com>
2020-05-02 11:45:17 +0200

    initial commit for text adventure.

--- adjektiv_historie.py
+++ adjektiv_historie.py
@@ -3,17 +3,8 @@ with open("historie.txt","r") as file:
     story = file.read()
     
 counts = story.count("PlaceHolder")
-#print(counts)    
+print(counts)  
 
-Adj=[]
-for element in range(counts):
-    Adj.append(input("Skriv adjektiv: "))
+for i in counts:
+    adj = input("Adj: ")
 
-PlaceHolder = "Placeholder"
-
-'''
-for placeholder in Story:
-    story.append in range(adj[::1])
-'''    
-               
-#print(Adj)
---
+++ text_adventure.py
@@ -0,0 +1,39 @@
+print("intro")
+print("intro2")
+
+print("lobby, to veier")
+
+def rom_1(path):
+    
+    while path != "1" and path != "2" and path != "3":
+        path = input("Hvilken vei vil du gå? rom1")
+    
+    if path == "1":
+        print("Rom1 vei 1")
+        path_rom1 = input("Hvilken vei? rom1_vei1 ") # rom 2 eller 3 
+    if path == "2":
+        print("Rom1 vei 2")
+        path_rom1 = input("Hvilken vei? rom1_vei2 ") # rom 2 eller 3 
+    if path == "3":
+        print("Død rom1_vei3")
+        return path_rom1
+
+print(path_rom1)   
+   
+def rom_2(path_rom1):
+    while path_rom1 != "1" and path_rom1 != "2":
+        path_rom2 = input("Hvilken vei? rom2")
+    
+    
+    if path_rom2 == "1":
+        print("Død rom2_vei1")
+       
+    if path_rom2 == "2":
+        print("win rom2_vei2")
+    return path_rom2
+
+#def rom_3(path_rom2):
+    #while path_rom1 != "2"
+print(path_rom2)
+rom_1("")
+rom_2("")
