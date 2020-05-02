dd870868b633b2882104feebe70b3e6d0104fabc
Nicolai Finstad Larsen <59092220+NicolaiFinstadLarsen@users.noreply.github.com>
2020-05-02 12:28:33 +0200

    trying to fix return statement.

--- text_adventure.py
+++ text_adventure.py
@@ -3,23 +3,24 @@ print("intro2")
 
 print("lobby, to veier")
 
-def rom_1(path):
+def rom_1(self, path):
     
     while path != "1" and path != "2" and path != "3":
         path = input("Hvilken vei vil du gå? rom1")
     
     if path == "1":
-        print("Rom1 vei 1")
+        print("Rom1_vei1")
         path_rom1 = input("Hvilken vei? rom1_vei1 ") # rom 2 eller 3 
     if path == "2":
-        print("Rom1 vei 2")
+        print("Rom1_vei2")
         path_rom1 = input("Hvilken vei? rom1_vei2 ") # rom 2 eller 3 
     if path == "3":
         print("Død rom1_vei3")
-        return path_rom1
+    return path_rom1
 
-print(path_rom1)   
-   
+main_path_rom1=rom_1("")
+print(main_path_rom1)    
+'''   
 def rom_2(path_rom1):
     while path_rom1 != "1" and path_rom1 != "2":
         path_rom2 = input("Hvilken vei? rom2")
@@ -35,5 +36,6 @@ def rom_2(path_rom1):
 #def rom_3(path_rom2):
     #while path_rom1 != "2"
 print(path_rom2)
+'''
 rom_1("")
-rom_2("")
+#rom_2("")
