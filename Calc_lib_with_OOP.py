c815b77d18d813abbce12879c920caa0400bbd10
Nicolai Finstad Larsen <59092220+NicolaiFinstadLarsen@users.noreply.github.com>
2020-05-07 08:25:40 +0200

    initial commit

--- Calc_lib_with_OOP.py
+++ Calc_lib_with_OOP.py
@@ -1,10 +1,19 @@
-class areas:
+class Areas:
     
     #width, height, base, side, 
     def __init__(self, w, h, b, s):
-        self.width = w
-        self.height = h
-        self.base = b
-        self.side = s
+        self.w = w
+        self.h = h
+        self.b = b
+        self.s = s
         
-        def square()
+    def square(self):
+        print("Area is:")
+        print(self.s**2)
+        
+    def Rectangle(w, h):
+            
+squareWithSide2 = Areas(0,0,0,2)
+
+
+squareWithSide2.square()
