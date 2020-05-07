df0fcc57f04868056f448dd357e06d2be15d3ab3
Nicolai Finstad Larsen <59092220+NicolaiFinstadLarsen@users.noreply.github.com>
2020-05-07 08:06:56 +0200

    initial commit

---
+++ Calc_lib_with_OOP.py
@@ -0,0 +1,10 @@
+class areas:
+    
+    #width, height, base, side, 
+    def __init__(self, w, h, b, s):
+        self.width = w
+        self.height = h
+        self.base = b
+        self.side = s
+        
+        def square()
