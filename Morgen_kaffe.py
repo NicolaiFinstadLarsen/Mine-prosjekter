6e5b35d161901ead70dc0818bf3d66b77d0a121a
Nicolai Finstad Larsen <59092220+NicolaiFinstadLarsen@users.noreply.github.com>
2020-05-09 10:01:11 +0200

    initial commit

--- Morgen_kaffe.py
+++ Morgen_kaffe.py
@@ -1,15 +1,22 @@
 import webbrowser
+import os
+
+webbrowser.open(url = "https://www.vg.no/", new=1, autoraise=True)
 
 
-print("Good morning :)")
+x = webbrowser.register(
+    "chrome2", None, "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+    )
+controller = webbrowser.get(x)
 
+#os.system("C:\Program Files (x86)\Google\Chrome\Application\chrome.exe")
 
-def open_papers():
+def papers():
     
-    webbrowser.open_new("https://www.vg.no/")
-    webbrowser.open("https://www.dagbladet.no/", new=2, autoraise=True)
-    webbrowser.open("https://www.nrk.no/", new=2, autoraise=True)
-    webbrowser.open("https://www.aftenposten.no/", new=2, autoraise=True)
-    webbrowser.open("https://e24.no/", new=2, autoraise=True)
+    controller.open_new("https://www.vg.no/")
+    #webbrowser.open("https://www.dagbladet.no/", new=0, autoraise=True)
+    #webbrowser.open("https://www.nrk.no/", new=2, autoraise=True)
+    #webbrowser.open("https://www.aftenposten.no/", new=2, autoraise=True)
+    #webbrowser.open("https://e24.no/", new=2, autoraise=True)
     
-open_papers()
+papers()
