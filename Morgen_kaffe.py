4955f471c138b6ff295eb2ea1f5a60601ccaf818
Nicolai Finstad Larsen <59092220+NicolaiFinstadLarsen@users.noreply.github.com>
2020-05-23 00:07:04 +0200

    "webbrowser" klarer ikke åpne et nytt vindu i chrome

--- Morgen_kaffe.py
+++ Morgen_kaffe.py
@@ -1,22 +1,20 @@
 import webbrowser
-import os
-
-webbrowser.open(url = "https://www.vg.no/", new=1, autoraise=True)
-
-
-x = webbrowser.register(
-    "chrome2", None, "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe %s"
-    )
-controller = webbrowser.get(x)
-
-#os.system("C:\Program Files (x86)\Google\Chrome\Application\chrome.exe")
 
 def papers():
     
-    controller.open_new("https://www.vg.no/")
-    #webbrowser.open("https://www.dagbladet.no/", new=0, autoraise=True)
-    #webbrowser.open("https://www.nrk.no/", new=2, autoraise=True)
-    #webbrowser.open("https://www.aftenposten.no/", new=2, autoraise=True)
-    #webbrowser.open("https://e24.no/", new=2, autoraise=True)
+    webbrowser.open_new("https://www.aftenposten.no/")
+    webbrowser.open("https://www.nrk.no/", new=2, autoraise=True)
+    webbrowser.open("https://e24.no/", new=2, autoraise=True)
+    webbrowser.open("https://www.dn.no/", new=2, autoraise=True)
+    
+    webbrowser.open(
+        "https://investor.nordea.no/oetoi/noi/bookmark?time=1590180670578&view=OPEN_MARKET_GLOBAL_VIEW&locale=no_NO&GENID=2ba4b997f2b44e8bbdc8d86f4a9a794d", new=2, autoraise=True
+        )
+    
+    webbrowser.open("https://investor.dn.no/#!/Oversikt", new=2, autoraise=True)
+    webbrowser.open("https://www.shareville.no/me/portfolios", new=2, autoraise=True)
+    webbrowser.open("https://www.vg.no/", new=1, autoraise=True)
+    webbrowser.open("https://www.dagbladet.no/", new=0, autoraise=True)
+    
     
 papers()
