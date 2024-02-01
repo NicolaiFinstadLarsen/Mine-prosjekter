768ba197c0d34982d5b6492793bae2bf614e5c30
Nicolai Finstad Larsen <nicolaifinstadlarsen@gmail.com>
2024-02-01 12:59:53 +0100

    Revert "no message"
    This reverts commit b383d4f12831fd7d221d5d8a37c0aad4ff09f064.

--- Morgen_kaffe.py
+++ Morgen_kaffe.py
@@ -1,25 +1,22 @@
 import webbrowser
 
-
+url = "https://www.nrk.no/"
 
 for i in range(1,2):
     
     chrome_path1 = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe --profile-directory="Profile '+str(i)+'" %s'
    
-    webbrowser.get(chrome_path1).open("https://www.nrk.no/")
+    webbrowser.get(chrome_path1).open(url)
     
-    webbrowser.open("https://e24.no/", new=0, autoraise=True)
+    webbrowser.open("https://e24.no/", new=2, autoraise=True)
     webbrowser.open("https://www.dn.no/", new=2, autoraise=True)
     
     webbrowser.open(
         "https://investor.nordea.no/oetoi/noi/bookmark?time=1590180670578&view=OPEN_MARKET_GLOBAL_VIEW&locale=no_NO&GENID=2ba4b997f2b44e8bbdc8d86f4a9a794d", new=2, autoraise=True
         )
-    webbrowser.open("https://www.nordnet.no/no", new=2, autoraise=True)
+    
     webbrowser.open("https://investor.dn.no/#!/Oversikt", new=2, autoraise=True)
     webbrowser.open("https://www.shareville.no/me/portfolios", new=2, autoraise=True)
-    webbrowser.open("https://www.euronextvps.no/", new=2, autoraise=True)
     webbrowser.open("https://www.vg.no/", new=2, autoraise=True)
     webbrowser.open("https://www.dagbladet.no/", new=2, autoraise=True)
-    
-    
     
--- prosjekt_lage_graf_i_console.py
+++
@@ -1,7 +0,0 @@
-
-for i in range(10,0,-1):
-    print(i, "/".rjust(i*2))
-    
-for i in range(0,11) :
-    print(i, end=" ")
-    
